0cd85ca6efd45c8f9323812d0fc477c540e93c4d
nuelife <mujeeb@nuelife.co.uk>
2021-02-14 19:30:23 +0100

    added main entry file

---
+++ ImageSplitter.py
@@ -0,0 +1,27 @@
+'''
+Image Splitter -> Split any image into multiple channels.
+RGBA image would be splitted into 4 channels.
+RGB would be splitted into 3 channels only.
+'''
+
+
+__author__  = 'mujeebishaque'
+__version__ = 'v1.0'
+
+
+class ImageSplitter:
+
+    def __init__(self):
+        pass
+
+    def determine_format(self):
+        pass
+    
+    @property
+    def number_of_channels(self):
+        pass
+    
+    @property
+    def image_format(self):
+        pass
+
---
+++ main.py
@@ -0,0 +1,7 @@
+try:
+    from ImageSplitter import ImageSplitter
+except ImportError:
+    raise ImportError("Couldn't Import ImageSpliiter class")
+
+if __name__ == '__main__':
+    pass
