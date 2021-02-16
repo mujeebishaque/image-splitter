49ea1f5b017dee5e07bf71293ceab354a6c949d0
mujeebishaque <mujeeb.ishaque@gmail.com>
2021-02-16 12:33:56 +0100

    removed redundancy

--- ImageSplitter.py
+++ ImageSplitter.py
@@ -20,22 +20,18 @@ class ImageSplitter:
 
     def __init__(self, file_path):
         if os.path.exists(str(file_path)) or os.path.isfile(str(file_path)):
-            self.image_file = Image.open(str(file_path))
+            self.image_file   = Image.open(str(file_path))
+            self.image_exists = True
         else:
             raise FileExistsError("Couldn't find file. Please provide a valid file path.")
 
-    def determine_format(self):
-        img = Image.open(self.image_file)
-        image_format = img.format
-        return str(image_format)
-    
     @property
     def number_of_channels(self):
-        return self.image_file.mode.__str__()
+        return len(self.image_file.mode.__str__())
     
     @property
     def image_format(self):
-        return self.determine_format().__str__()
+        return self.image_file.format.__str__()
 
     def image_channels(self):
         data   = self.image_file.getdata()
